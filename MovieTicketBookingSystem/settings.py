bf5d04526be443112285cdc67511413cfd77b99d
Anshul Agrawal <aanshul543@gmail.com>
2021-09-05 23:49:27 +0530

    Settings Update

--- MovieTicketBookingSystem/settings.py
+++ MovieTicketBookingSystem/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
         'USER': config('DATABASE_USER'),
         'PASSWORD': config('DATABASE_PASSWORD'),
         'HOST': config('DATABASE_HOST'),
-        'PORT': '',
+        'PORT': config('DATABASE_PORT'),
     }
 }
 
